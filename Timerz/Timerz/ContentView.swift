7114706f25910f522f297d1e2aa0e865ae9cacfd
Kyle Young <dev.kyoung@gmail.com>
2022-03-13 23:58:51 -0700

    ui only basic view

--- Timerz/Timerz/ContentView.swift
+++ Timerz/Timerz/ContentView.swift
@@ -7,10 +7,67 @@
 
 import SwiftUI
 
+struct Timers: View {
+    var body: some View {
+        VStack {
+            Timers_Controls()
+            Timers_Body()
+        }
+    }
+}
+
+struct Timers_Controls: View {
+    @State private var timeToAdd = Date()
+    @State private var showAlert: Bool = false
+    var body: some View {
+        HStack {
+            DatePicker("", selection: $timeToAdd, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+            Button("Add Timer") {
+                showAlert = true
+            }
+                .alert("Important message", isPresented: $showAlert) {
+                Button("OK", role: .cancel) { }
+            }
+        }
+    }
+}
+
+struct Timers_Body: View {
+    var body: some View {
+        VStack {
+            Timer()
+            Timer()
+            Timer()
+            Timer()
+        }
+    }
+}
+
+struct Timer: View {
+    var body: some View {
+        VStack {
+            Timer_Time()
+            Timer_Controls()
+        }
+    }
+}
+
+struct Timer_Time: View {
+    var body: some View {
+        Text("Time: 30")
+    }
+}
+
+struct Timer_Controls: View {
+    var body: some View {
+        Text("Start | Stop | Delete")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Timers()
     }
 }
 
