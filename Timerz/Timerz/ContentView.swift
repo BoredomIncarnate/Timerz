8b131f31fe6917bb5fd19d040a7a4c3b5ede9aca
Kyle Young <dev.kyoung@gmail.com>
2022-03-13 23:30:08 -0700

    skeleton project

---
+++ Timerz/Timerz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timerz
+//
+//  Created by Kyle Young on 3/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timerz/Timerz/TimerzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerzApp.swift
+//  Timerz
+//
+//  Created by Kyle Young on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
